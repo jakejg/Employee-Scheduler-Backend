9531cdb289ecd57c63fb2aa198d1ad04231c652e
Jake <jakefrisbee@gmail.com>
2022-01-20 20:25:18 -0700

    change file structure

--- front-end/src/config.js
+++
@@ -1,2 +0,0 @@
-export const BASE_URL = process.env.REACT_APP_BASE_URL || `http://localhost:5000`;
-export const drawerWidth = window.innerWidth > 500 ? 240 : 150;
--- front-end/src/helpers/underscoreName.js
+++
@@ -1,5 +0,0 @@
-function underscoreName(name){
-    return name.toLowerCase().replace(/\s/g, '_');
-}
-
-export default underscoreName;
