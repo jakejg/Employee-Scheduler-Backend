d2d00a6738c45e6d3602119c8dc059d85ecf3822
Jake <jakefrisbee@gmail.com>
2022-01-21 08:25:40 -0700

    reject unathorize false

--- db.js
+++ db.js
@@ -2,8 +2,10 @@ const { DB_URI } = require('./config');
 const { client, Client } = require('pg');
 
 let db = new Client({
-        connectionString: DB_URI
+        connectionString: DB_URI,
+        rejectUnauthorized: false
     })
 db.connect();
+console.log("Database connected")
 
 module.exports = db;
